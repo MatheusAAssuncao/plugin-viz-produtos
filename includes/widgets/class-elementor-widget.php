b69afd92da15ee96d4066b8bf9c9653665ff7a72
Matheus <massuncao@digitalsales.pt>
2024-11-26 15:52:42 +0000

    Testes paginação

--- includes/widgets/class-elementor-widget.php
+++ includes/widgets/class-elementor-widget.php
@@ -129,7 +129,6 @@ class Viz_Produtos_Elementor_Widget extends \Elementor\Widget_Base
             }
 
             echo '</div>';
-            echo '<button id="load-more">Carregar mais</button>';
         } else {
             echo __('Nenhum produto encontrado.', 'viz-plugin-produtos');
         }
