fd58139d65d01d0ba6ced0a3ed287b40cb5f0946
Matheus <massuncao@digitalsales.pt>
2024-11-27 15:15:00 +0000

    Pagination

--- assets/js/script.js
+++ assets/js/script.js
@@ -5,23 +5,23 @@ jQuery(document).ready(function($) {
         var postsPerPage = button.data("posts-per-page");
         console.log(ajax_object.ajax_url)
         console.log(ajax_object.viz_products_nonce)
-        $.ajax({
-            url: ajax_object.ajax_url,
-            type: "POST",
-            data: {
-                action: "load_more_products",
-                page: page + 1,
-                posts_per_page: postsPerPage,
-                nonce: ajax_object.viz_products_nonce
-            },
-            success: function(response) {
-                if (response) {
-                    $(".viz-widget-produtos").append(response);
-                    button.data("page", page + 1);
-                } else {
-                    button.text("No more products").prop("disabled", true);
-                }
-            }
-        });
+        // $.ajax({
+        //     url: ajax_object.ajax_url,
+        //     type: "POST",
+        //     data: {
+        //         action: "load_more_products",
+        //         page: page + 1,
+        //         posts_per_page: postsPerPage,
+        //         nonce: ajax_object.viz_products_nonce
+        //     },
+        //     success: function(response) {
+        //         if (response) {
+        //             $(".viz-widget-produtos").append(response);
+        //             button.data("page", page + 1);
+        //         } else {
+        //             button.text("No more products").prop("disabled", true);
+        //         }
+        //     }
+        // });
     });
 });
